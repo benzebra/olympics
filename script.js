668f3561da0d5c073c4005e9ae5a5f80338a6e6d
benzebra <filippobrajucha@gmail.com>
2024-08-15 10:39:00 +0200

    landing

--- script.js
+++ script.js
@@ -13,21 +13,22 @@ var shininess; //Ns
 var opacity;   //Ni
 
 function main() {
-	// Get A WebGL context
-	/** @type {HTMLCanvasElement} */
-	var canvas = document.getElementById("canvas");
+	var canvas = document.getElementById("my-canvas");
 	var gl = canvas.getContext("webgl");
 	if (!gl) {
 		return;
 	}
-
+	console.log(mesh)
 	// mesh.sourceMesh = 'resources/obj/torch/torch.obj';
     //mesh.sourceMesh = 'resources/obj/olympics-torch/source/TO SUBSTANCE .obj';
-    //mesh.sourceMesh = 'resources/obj/soccerball/soccerball.obj';
+    // mesh.sourceMesh = 'resources/obj/soccerball/soccerball.obj';
 	// mesh.sourceMesh = 'resources/obj/girl/Sport_girl.obj';
-	//mesh.sourceMesh = 'resources/obj/Painter1/Shoes--HH-A1.obj';
+	// mesh.sourceMesh = 'resources/obj/Painter1/Shoes--HH-A1.obj';
 	mesh.sourceMesh = 'resources/obj/logo/model.obj';
-	LoadMesh(gl,mesh);
+	// mesh.sourceMesh = 'resources/obj/lady/Ledy_Sport.obj';
+	// mesh.sourceMesh = 'resources/obj/chair/chair.obj';
+	console.log(mesh)
+	LoadMesh(gl, mesh);
 	//console.log(mesh);
 
 	// setup GLSL program
@@ -37,21 +38,17 @@ function main() {
 
 	// look up where the vertex data needs to go.
 	var positionLocation = gl.getAttribLocation(program, "a_position");
-	var normalLocation = gl.getAttribLocation(program, "a_normal");
+	var normalLocation   = gl.getAttribLocation(program, "a_normal");
 	var texcoordLocation = gl.getAttribLocation(program, "a_texcoord");
 
-	// Create a buffer for positions
+	// Create a buffer for normals, bind it to ARRAY_BUFFER and put the positions in the buffer
 	var positionBuffer = gl.createBuffer();
-	// Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-	// Put the positions in the buffer
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-	// Create a buffer for normals
+	// Create a buffer for normals, bind it to ARRAY_BUFFER and put the normals in the buffer
 	var normalsBuffer = gl.createBuffer();
-	// Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER mormalsBuffer)
 	gl.bindBuffer(gl.ARRAY_BUFFER, normalsBuffer);
-	// Put the normals in the buffer
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
 
 	// provide texture coordinates
@@ -60,14 +57,14 @@ function main() {
 	// Set Texcoords
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texcoords), gl.STATIC_DRAW);
 
-	var ambientLight=[0.2,0.2,0.2];
-	var colorLight=[1.0,1.0,1.0];
+	var ambientLight = [ 0, 0, 0 ];
+	var colorLight	 = [ 1.0, 1.0, 1.0 ];
 
 	gl.uniform3fv(gl.getUniformLocation(program, "diffuse" ), diffuse );
 	gl.uniform3fv(gl.getUniformLocation(program, "ambient" ), ambient); 
 	gl.uniform3fv(gl.getUniformLocation(program, "specular"), specular );	
 	gl.uniform3fv(gl.getUniformLocation(program, "emissive"), emissive );
-	//gl.uniform3fv(gl.getUniformLocation(program, "u_lightDirection" ), xxx );
+	// gl.uniform3fv(gl.getUniformLocation(program, "u_lightDirection" ), xxx );
 	gl.uniform3fv(gl.getUniformLocation(program, "u_ambientLight" ), ambientLight );
 	gl.uniform3fv(gl.getUniformLocation(program, "u_colorLight" ), colorLight );
 
@@ -104,14 +101,22 @@ function main() {
 	var modelXRotationRadians = degToRad(0);
 	var modelYRotationRadians = degToRad(0);
 
+	var radius = 3.50;
+
 	// Compute the projection matrix
 	var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
 	//  zmin=0.125;
-	var zmin=0.1;
-	var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, zmin, 200);
+	// var zmin = 0.1;
+	var zmin = radius/100;
+	var zmax = radius*3;
+	var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, zmin, zmax);
 
-	var cameraPosition = [4.5, 4.5, 2];
-	var up = [0, 0, 1];
+	// var extents = getGeometriesExtents(obj.geometries);
+	// var range = m4.subtractVectors(extents.max, extents.min);
+	
+
+	var cameraPosition = [0, 0, 1.5 * radius];
+	var up = [0, 1, 0];
 	var target = [0, 0, 0];
 
 	// Compute the camera's matrix using look at.
@@ -120,12 +125,12 @@ function main() {
 	// Make a view matrix from the camera matrix.
 	var viewMatrix = m4.inverse(cameraMatrix);
 
-	var matrixLocation = gl.getUniformLocation(program, "u_world");
-	var textureLocation = gl.getUniformLocation(program, "diffuseMap");
-	var viewMatrixLocation = gl.getUniformLocation(program, "u_view");
-	var projectionMatrixLocation = gl.getUniformLocation(program, "u_projection");
+	var matrixLocation 				= gl.getUniformLocation(program, "u_world");
+	var textureLocation 			= gl.getUniformLocation(program, "diffuseMap");
+	var viewMatrixLocation 			= gl.getUniformLocation(program, "u_view");
+	var projectionMatrixLocation 	= gl.getUniformLocation(program, "u_projection");
 	var lightWorldDirectionLocation = gl.getUniformLocation(program, "u_lightDirection");
-	var viewWorldPositionLocation = gl.getUniformLocation(program, "u_viewWorldPosition");
+	var viewWorldPositionLocation 	= gl.getUniformLocation(program, "u_viewWorldPosition");
 
 	gl.uniformMatrix4fv(viewMatrixLocation, false, viewMatrix);
 	gl.uniformMatrix4fv(projectionMatrixLocation, false, projectionMatrix);
@@ -152,6 +157,9 @@ function main() {
 
 	// Draw the scene.
 	function drawScene(time) {
+
+		webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
 		// convert to seconds
 		time *= 0.001;
 		// Subtract the previous time from the current time
@@ -166,7 +174,7 @@ function main() {
 		gl.enable(gl.DEPTH_TEST);
 
 		// Animate the rotation
-		modelYRotationRadians += -0.7 * deltaTime;
+		modelYRotationRadians += -0.5 * deltaTime;
 		modelXRotationRadians += -0.4 * deltaTime;
 
 		// Clear the canvas AND the depth buffer.
