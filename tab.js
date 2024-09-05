0f80cfe37d0e90a60ef600e1a25115c9a240dd34
benzebra <filippobrajucha@gmail.com>
2024-09-05 18:46:53 +0200

    upadtes

--- tab.js
+++ tab.js
@@ -1,18 +1,29 @@
-var srcImages = ["resources/img/sword.avif", "resources/img/volley.avif", "resources/img/kayak.avif", "resources/img/tennis.avif"];
-var figures = ["sword", "volleyball", "kayak", "tennis"];
+var srcImages = [
+    "resources/img/sword.avif", 
+    "resources/img/volley.avif", 
+    "resources/img/kayak.avif", 
+    "resources/img/tennis.avif"];
+
+var figures = [
+    "sword", 
+    "volleyball", 
+    "kayak", 
+    "tennis"
+];
+
 var gl;
 var gui;
 var value;
 
-var controls = {
-	near : 1,
-	far : 100,
-	d : 8.5,
-	fov : 40.0,  
-	theta_light : degToRad(20),
-	phi_light  : degToRad(80),
-	d_light : 8.5,
-};
+// var controls = {
+// 	near : 1,
+// 	far : 100,
+// 	d : 8.5,
+// 	fov : 40.0,  
+// 	theta_light : degToRad(20),
+// 	phi_light  : degToRad(80),
+// 	d_light : 8.5,
+// };
 
 document.addEventListener('DOMContentLoaded', function() {
     var canvas = document.getElementById("my-canvas");
@@ -21,14 +32,14 @@ document.addEventListener('DOMContentLoaded', function() {
 		return;
 	}
 
-    gui = new dat.GUI();
-    gui.add(controls, "near", 			1,		10, 	1).onChange(function() {render(value, gl, controls, false)});
-    gui.add(controls, "far", 			1, 		100, 	1).onChange(function() {render(value, gl, controls, false)});
-    gui.add(controls, "d", 				0, 		10, 	1).onChange(function() {render(value, gl, controls, false)});
-    gui.add(controls, "fov", 			10, 	120, 	5).onChange(function() {render(value, gl, controls, false)});
-    // gui.add(controls, "theta_light", 	1, 		6.28, 	dr).onChange(function() {render(value, gl, controls, false)});
-    // gui.add(controls, "phi_light", 		1, 		10, 	dr).onChange(function() {render(value, gl, controls, false)});
-    gui.add(controls, "d_light", 		1.75, 	10, 	1).onChange(function() {render(value, gl, controls, false)});
+    // gui = new dat.GUI();
+    // gui.add(controls, "near", 			1,		10, 	1).onChange(function() {render(value, gl, controls, false)});
+    // gui.add(controls, "far", 			1, 		100, 	1).onChange(function() {render(value, gl, controls, false)});
+    // gui.add(controls, "d", 				0, 		10, 	1).onChange(function() {render(value, gl, controls, false)});
+    // gui.add(controls, "fov", 			10, 	120, 	5).onChange(function() {render(value, gl, controls, false)});
+    // // gui.add(controls, "theta_light", 	1, 		6.28, 	dr).onChange(function() {render(value, gl, controls, false)});
+    // // gui.add(controls, "phi_light", 		1, 		10, 	dr).onChange(function() {render(value, gl, controls, false)});
+    // gui.add(controls, "d_light", 		1.75, 	10, 	1).onChange(function() {render(value, gl, controls, false)});
 
     // attach to each td element a link to google
     tdArray = document.getElementsByTagName("td");
@@ -42,19 +53,18 @@ document.addEventListener('DOMContentLoaded', function() {
 function imgClick(text) {
     switch(text){
         case "sword":
-            value = 'resources/obj/sword/medalist.obj';
+            objHref = 'resources/obj/logo/model.obj';
             break;
         case "volleyball":
-            value = 'resources/obj/volleyball/Volleyball.obj';
+            objHref = 'resources/obj/logo_2024/logo.obj';
             break;
         case "kayak":
-            value = 'resources/obj/kayak/30daysinVRkayak.obj';
+            objHref = 'resources/obj/kayak/30daysinVRkayak.obj';
             break;
         case "tennis":
-            value = 'resources/obj/tennis-racket/06-12-19_tennis_racket_export_v1.obj';
+            objHref = 'resources/obj/tennis2/tennis.obj';
             break;
     }
 
-    render(value, gl);
-    // render(value, gl, controls, true);
+    loaderMain()
 }
