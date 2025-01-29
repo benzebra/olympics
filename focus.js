1f4fbdc0df0a4d0eae33460e6f9fbb8708cbc020
benzebra <filippobrajucha@gmail.com>
2025-01-29 16:43:19 +0100

    feat: controls bug fix

--- focus.js
+++ focus.js
@@ -16,10 +16,6 @@ main(index, gl, meshProgramInfo, true, canvas);
 
 const gui = new dat.GUI();
 gui.name = "Controls";
-// gui.closed = true;
-// gui.autoPlace = true;
-gui.add(controls, "posX", 	    0,		            10,             1);
-gui.add(controls, "posY", 	    0, 		            10, 	        1);
 gui.add(controls, "D", 		    0, 		            30, 	        1);
 gui.add(controls, "THETA", 	    0, 	                2*Math.PI, 	    0.1);
 gui.add(controls, "PHI", 	    -Math.PI/2+0.1,     Math.PI/2-0.1, 	0.1);
@@ -37,13 +33,6 @@ function handleDatGuiVisibility() {
         guiContainer.style.display = ''; // Show the GUI on desktop
     }
 }
-
-// function loadArticle(i) {
-//     content = fetch(urlRes + i + ".html");
-//     content.then(response => response.text()).then(text => {
-//         document.getElementById("article").innerHTML = text;
-//     });
-// }
 
 handleDatGuiVisibility()
 
